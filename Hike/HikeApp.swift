1f2fbc438e96add388940f041efbb2dcf61bbf52
Paul Onawola <onawola.ire@gmail.com>
2024-05-08 16:52:05 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Paul Onawola on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
